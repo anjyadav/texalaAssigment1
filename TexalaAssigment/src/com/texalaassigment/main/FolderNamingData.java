6260f82cdeecd04e876b503f613a4afb0f97f7c7
anjyadav <92594702+anjyadav@users.noreply.github.com>
2022-01-29 15:36:41 +0530

    Add files via upload

---
+++ TexalaAssigment/src/com/texalaassigment/main/FolderNamingData.java
@@ -0,0 +1,8 @@
+package com.texalaassigment.main;
+
+public interface FolderNamingData {
+	String filePathFolder =  "F:\\Texala";
+	String filePathSubFolder ="assigment1";
+	String filePathHtmlFolder = "AllReadedHtmlData";
+
+}
