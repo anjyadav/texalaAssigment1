ff94eb80b724814b9a73e187b003e72430852fdc
anjyadav <92594702+anjyadav@users.noreply.github.com>
2022-01-31 00:18:43 +0530

    Delete TexalaAssigment directory

--- TexalaAssigment/src/com/texalaassigment/main/FolderNamingData.java
+++
@@ -1,8 +0,0 @@
-package com.texalaassigment.main;
-
-public interface FolderNamingData {
-	String filePathFolder =  "F:\\Texala";
-	String filePathSubFolder ="assigment1";
-	String filePathHtmlFolder = "AllReadedHtmlData";
-
-}
